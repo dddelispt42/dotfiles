8557a5dbbec0df379f6e74404625d9b5114f38db
Riemer, Heiko (Coriant - PT/Amadora) <heiko.riemer@coriant.com>
2019-08-02 15:14:35 +0100

    use lsd (vs. ls) if available and switch fuzzy functions to rg (vs. ag)

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,10 +1,10 @@
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     # test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    # alias ls='ls --color=auto'
     # alias ls='ls --color=always'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
+    # alias dir='dir --color=auto'
+    # alias vdir='vdir --color=auto'
 
     alias grep='grep --color=auto'
     # alias grep='grep --color=always'
