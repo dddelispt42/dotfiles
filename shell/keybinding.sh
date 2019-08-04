3deda19c062851ee565c2c08dd6802621c98ec83
Heiko Riemer <heiko@eheiko.net>
2019-08-04 21:17:27 +0100

    some small zsh keybinding fixes

--- shell/keybinding.sh
+++ shell/keybinding.sh
@@ -1,1 +1,4 @@
-bindkey '\e.' insert-last-word
+bindkey '\e.' insert-last-word # Esc-. to insert last word from prev cmd
+bindkey '^P' up-line-or-history # Ctrl-N to scroll down history
+bindkey '^N' down-line-or-history # Ctrl-P to scroll up history
+bindkey '^[[A' fzf-history-widget # Ctrl-R to reverse search history w/ fzf
