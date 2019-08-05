bedc4c50ed5f7abf3f438f80b4689f428dbdd017
Heiko Riemer <heiko@eheiko.net>
2019-08-05 19:31:03 +0100

    fix ls/lsd in function issues

--- shell/ssh_agent.sh
+++ shell/ssh_agent.sh
@@ -14,7 +14,7 @@ fi;
 test -e "$HOME/.agent.sh" && source "$HOME/.agent.sh"
 
 function keyadd() {
-    ssh-add $(diff <(ssh-add -L | sed -e 's/.* //'|sort) <(ls $HOME/.ssh/*id_* | grep -v "\.pub$"|sort) | grep --color=no -E "[<>] " | sed -e "s/[<>] //" | fzf -x)
+    ssh-add $(diff <(ssh-add -L | sed -e 's/.* //'|sort) <(\ls $HOME/.ssh/*id_* | grep -v "\.pub$"|sort) | grep --color=no -E "[<>] " | sed -e "s/[<>] //" | fzf -x)
 }
 function keyremove() {
     ssh-add -d "$(ssh-add -L | sed -e 's/.* //' | fzf -x)"
