381c8cf1b4111233f5d65db1f077bd884d1bd973
Heiko Riemer <heiko@eheiko.net>
2019-11-22 21:31:16 +0000

    add xdg-mime defaults

---
+++ xdg-mime.sh
@@ -0,0 +1,67 @@
+xdg-mime default  application/gpx+xml
+xdg-mime default  application/gzip
+xdg-mime default  application/javascript
+xdg-mime default  application/json
+xdg-mime default  application/mbox
+xdg-mime default  application/octet-stream
+xdg-mime default gimp.desktop application/postscript
+xdg-mime default  application/sql
+xdg-mime default  application/x-awk
+xdg-mime default  application/x-desktop
+xdg-mime default  application/x-executable
+xdg-mime default  application/x-font-afm
+xdg-mime default  application/x-font-tex
+xdg-mime default  application/x-font-tex-tfm
+xdg-mime default  application/x-font-type1
+xdg-mime default  application/x-genesis-rom
+xdg-mime default  application/x-java-keystore
+xdg-mime default  application/x-m4
+xdg-mime default  application/xml
+xdg-mime default  application/xml-dtd
+xdg-mime default  application/xml-external-parsed-entity
+xdg-mime default  application/x-mozilla-bookmarks
+xdg-mime default  application/x-perl
+xdg-mime default  application/x-php
+xdg-mime default  application/x-ruby
+xdg-mime default  application/x-sharedlib
+xdg-mime default vim.desktop application/x-shellscript
+xdg-mime default  application/x-trash
+xdg-mime default  application/x-x509-ca-cert
+xdg-mime default  application/x-yaml
+xdg-mime default  audio/x-mod
+xdg-mime default mplayer.desktop audio/x-wav
+xdg-mime default kde-kview.desktop image/jpeg
+xdg-mime default gimp.desktop image/svg+xml
+xdg-mime default  image/vnd.zbrush.pcx
+xdg-mime default  image/x-eps
+xdg-mime default  text/css
+xdg-mime default  text/csv
+xdg-mime default chromium-devel.desktop text/html
+xdg-mime default  text/markdown
+xdg-mime default vim.desktop text/plain
+xdg-mime default  text/rust
+xdg-mime default  text/troff
+xdg-mime default  text/x-authors
+xdg-mime default  text/x-bibtex
+xdg-mime default  text/x-changelog
+xdg-mime default vim.desktop text/x-chdr
+xdg-mime default vim.desktop text/x-c++hdr
+xdg-mime default  text/x-copying
+xdg-mime default  text/x-credits
+xdg-mime default vim.desktop text/x-csrc
+xdg-mime default vim.desktop text/x-c++src
+xdg-mime default  text/x-emacs-lisp
+xdg-mime default  text/x-go
+xdg-mime default  text/x-install
+xdg-mime default  text/x-iptables
+xdg-mime default vim.desktop text/x-java
+xdg-mime default  text/x-lua
+xdg-mime default vim.desktop text/x-makefile
+xdg-mime default  text/x-matlab
+xdg-mime default  text/x-microdvd
+xdg-mime default  text/x-ms-regedit
+xdg-mime default  text/x-patch
+xdg-mime default  text/x-python
+xdg-mime default  text/x-readme
+xdg-mime default  text/x-setext
+xdg-mime default vim.desktop text/x-tex
