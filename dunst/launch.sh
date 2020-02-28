b0196feaf8ac000a60a016389722d4762f714e1d
Riemer, Heiko <hriemer@infinera.com>
2020-02-28 17:19:50 +0000

    fix bspwm script to work at work

--- dunst/launch.sh
+++ dunst/launch.sh
@@ -4,7 +4,7 @@
 killall -q dunst
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x dunst >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/dunst.log /tmp/dunst.log
