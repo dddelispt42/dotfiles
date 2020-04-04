87a853835c8cc33c132e411737065f9b1b4cb916
Heiko Riemer <heiko@eheiko.net>
2020-04-04 22:27:10 +0100

    extend filetype coverage of lf previewer

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,15 +1,49 @@
 #!/bin/sh
+LINES=100
+
+# dia
+# docm
+# epub
+# icns
+# ICO
+# img
+# iso
+# potx
+# ppsx
+# psd
+# pptm
+# spt
+# vcm
+# vsd
+# VSD
+# xcf
+# xlsm
+# xps
+# MS Office docs
+
+if [ -d "$1" ]; then
+    ls -la "$1"
+    exit
+fi
 
 case "$1" in
-    *.tar*) tar tf "$1";;
-    *.zip) unzip -l "$1";;
-    *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
-    *.pdf) pdftotext "$1" -;;
-    *.jpg|.jpeg|*.png) termimage "$1";;
-    *.md) mdcat "$1";;
-    *.mp4|*.mkv) mediainfo "$1";;
-    *.sh) bat --color always "$1";;
-    *.py) bat --color always "$1";;
+    *.epub|*.EPUB) epub2txt "$1" | head -$LINES;;
+    *.ch|*.y) bat -l c "$1" || cat "$1";;
+        *.db|*.sqlite|*.DB|*.SQLITE) sqlite3 "$1" .schema | head -$LINES;;  # TODO improve and make more useful
+    # *.jpg|*.jpeg|*.png) termimage -a truecolor -s 70x70 "$1";;
+    *.jpg|*.JPG|*.jpeg|*.JPEG|*.png|*.PNG|*.tif|*.tiff|*.TIF|*.TIFF|.git|*.GIF) chafa "$1";;
+    *.lz4|.LZ4) lz4 --list "$1";;
+    *.md|*.MD|*.markdown) mdcat "$1" || glow "$1" || bat "$1" || highlight -O ansi --force "$1" || cat "$1";;
+    *.odt|*.ODT|*.ods|*.ODS|*.odp|*.ODP) odt2txt "$1";;
+    *.ogv|*.OGV|*.mp4|*.MP4|*.mkv|*.MKV|*.avi|*.AVI|*.swf|*.SWF|*.flv|*.FLV|*.mov|*.MOV|*.mpg|*.MPG|*.webm|*.WEBM) mediainfo "$1";;
+    *.pdf|*.PDF) pdftotext "$1" -;;
+    *.rar|*.RAR) unrar l "$1";;
+    *.smd|*.gliffy) bat -l json "$1" || cat "$1";;
+    *.tar*|*.TAR*) tar tf "$1";;
+    *.tex|*.bbl|*.bib|*.vrb|*.toc|*.snm|*.nav) bat -l tex "$1" || cat "$1";;
+    *.tmpl) bat -l html "$1" || cat "$1";;
+    *.wadl|*.proj) bat -l xml "$1" || cat "$1";;
+    *.zip|*.zipx|*.ZIP|*.ZIPX) unzip -l "$1";;
     *) bat --color always "$1" || highlight -O ansi --force "$1" || cat "$1";;
 esac
