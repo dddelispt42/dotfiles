6ad751606838230ad6d566f6283cb4393459f60e
Heiko Riemer <heiko@eheiko.net>
2020-06-13 15:47:09 +0100

    add rga-fzf fuzzy function

--- shell/fuzzy_functions.sh
+++ shell/fuzzy_functions.sh
@@ -451,3 +451,16 @@ function fbookm {
     fi
 }
 
+rga-fzf() {
+    RG_PREFIX="rga --files-with-matches"
+    local file
+    file="$(
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+            fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+                --phony -q "$1" \
+                --bind "change:reload:$RG_PREFIX {q}" \
+                --preview-window="70%:wrap"
+    )" &&
+    echo "opening $file" &&
+    xdg-open "$file"
+}
