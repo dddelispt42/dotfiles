06af704452b18bd421b084d14074e25448bd31a8
Riemer, Heiko <hriemer@infinera.com>
2020-09-10 21:29:51 +0100

    remove lfcd

--- lf/bootstrap.sh
+++ lf/bootstrap.sh
@@ -2,5 +2,5 @@
 BASEDIR=$(pwd)/$(dirname "$0")
 source ../dotfile_functions.sh
 
-create_dotfile_link "${BASEDIR}/lfcd.sh" ~/opt/lf
+# create_dotfile_link "${BASEDIR}/lfcd.sh" ~/opt/lf
 create_dotfile_link "${BASEDIR}" ~/.config/lf
