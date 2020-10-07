232cdea146d788f3a0e3ee60787ecca90dff185c
Riemer, Heiko <hriemer@infinera.com>
2020-10-07 12:35:02 +0100

    fix fssh fuzzy function for autossh

--- shell/ssh_agent.sh
+++ shell/ssh_agent.sh
@@ -2,7 +2,9 @@ if [ "$(pgrep -u "$USER" ssh-agent)" = "" ]; then
     test -e "$HOME/.cache/sshagent.sh" && rm -f "$HOME/.cache/sshagent.sh"
     ssh-agent | grep -v echo > "$HOME/.cache/sshagent.sh"
 fi
-test -e "$HOME/.cache/sshagent.sh" && . "$HOME/.cache/sshagent.sh"
+if [ -e "$HOME/.cache/sshagent.sh" ]; then
+    source "$HOME/.cache/sshagent.sh"
+fi
 
 keyadd() {
     ssh-add $(diff <(ssh-add -L | sed -e 's/.* //'|sort) <(\ls "$HOME"/.ssh/*id_* | grep -v "\.pub$"|sort) | grep --color=no -E "[<>] " | sed -e "s/[<>] //" | fzf -x)
