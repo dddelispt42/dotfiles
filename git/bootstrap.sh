240dfa9b53a30386aa8b25a00c4497cc721ac831
Heiko Riemer <heiko@eheiko.net>
2020-10-11 19:17:21 +0100

    more migration to XDG and stow

--- git/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BASEDIR=$(pwd)/$(dirname "$0")
-source ../dotfile_functions.sh
-
-if [ "$OS" == "Windows" ]; then
-    cp "${BASEDIR}/gitconfig" ~/.gitconfig
-else
-    create_dotfile_link "${BASEDIR}/gitconfig" ~/.gitconfig
-fi
