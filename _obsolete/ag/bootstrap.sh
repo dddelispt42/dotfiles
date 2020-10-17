46ec19ba6a568069c461f2db91da25c8c0626122
Heiko Riemer <heiko@eheiko.net>
2020-10-17 18:59:59 +0100

    remove obsolete .dotfiles

--- _obsolete/ag/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BASEDIR=$(dirname $(realpath "$0"))
-source ../dotfile_functions.sh
-
-create_dotfile_link "${BASEDIR}/ignore" ~/.ignore
-create_dotfile_link "${BASEDIR}/ignore" ~/.agignore
-create_dotfile_link "${BASEDIR}/ignore" ~/.rgignore
-create_dotfile_link "${BASEDIR}/ignore" ~/.gitignore
--- _obsolete/screen/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BASEDIR=$(dirname $(realpath "$0"))
-source ../dotfile_functions.sh
-
-create_dotfile_link "${BASEDIR}/screenrc" ~/.screenrc
