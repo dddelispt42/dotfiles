d883e846a52bff3d1540c9bb3eb54e588193bebc
Riemer, Heiko <hriemer@infinera.com>
2020-10-17 19:13:30 +0100

    add an uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+stow -vD -t ~/ shell
+stow -vD -t ~/ zsh
+stow -vD -t ~/ bat
+stow -vD -t ~/ bspwm
+stow -vD -t ~/ dunst
+stow -vD -t ~/ git
+stow -vD -t ~/ lf
+stow -vD -t ~/ polybar
+stow -vD -t ~/ sxhkd
+stow -vD -t ~/ tmux
+stow -vD -t ~/ nvim
+stow -vD -t ~/ X11
+
