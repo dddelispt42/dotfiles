b68f7f98db10adb8e889080a89046d99c4b946f9
Riemer, Heiko <hriemer@infinera.com>
2021-02-03 21:47:49 +0000

    fix pamixer for bluetooth and impl. xrandr.sh

--- dwm/.config/dwm/xrandr.sh
+++ dwm/.config/dwm/xrandr.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 intern=eDP1
-extern=HDMI1
-extern2=DP1
-extern3=HDMI2
+extern1=DP1
+extern2=DP2
+extern3=HDMI1
+extern4=HDMI2
 
-if xrandr | grep "^$extern connected" > /dev/null; then
-    xrandr --output "$extern" --primary --auto --output "$intern" --off
+if xrandr | grep "^$extern1 connected" > /dev/null; then
+    xrandr --output "$extern1" --primary --mode 3840x2160 --rate 30.00 --output "$intern" --off
 elif xrandr | grep "^$extern2 connected" > /dev/null; then
-    xrandr --output "$extern2" --primary --auto --output "$intern" --off
+    xrandr --output "$extern2" --primary --mode 3840x2160 --rate 30.00 --output "$intern" --off
 elif xrandr | grep "^$extern3 connected" > /dev/null; then
+    xrandr --output "$extern4" --primary --auto --output "$intern" --off
+elif xrandr | grep "^$extern4 connected" > /dev/null; then
     xrandr --output "$extern3" --primary --auto --output "$intern" --off
 else
-    xrandr --output "$extern" --off --output "$extern2" --off --output "$extern3" --off --output "$intern" --primary --auto
+    xrandr --output "$intern" --primary --auto
 fi
 
 # update dwmblocks
