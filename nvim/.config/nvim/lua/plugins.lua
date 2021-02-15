a974bb7ca51a63c012bb94f003579ccbd71f8be9
Riemer, Heiko <hriemer@infinera.com>
2021-02-15 09:18:13 +0000

    add old vimscript plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -78,13 +78,14 @@ return require('packer').startup {
     use 'wbthomason/lsp-status.nvim'
     use 'haorenW1025/completion-nvim'
     use {
-      'nvim-treesitter/nvim-treesitter'
+      'nvim-treesitter/nvim-treesitter',
       run = function() vim.cmd [[TSUpdate]] end
     }
     use {
       'nvim-treesitter/completion-treesitter',
       run = function() vim.cmd [[TSUpdate]] end
     }
+    use 'SirVer/ultisnips'
     use 'norcalli/snippets.nvim'
     use 'norcalli/ui.nvim'
     use 'liuchengxu/vista.vim'
@@ -103,9 +104,115 @@ return require('packer').startup {
     use 'tpope/vim-commentary'     -- Easily comment out lines or objects
     use 'tpope/vim-surround'       -- Surround text objects easily
     -- Floating windows are awesome :)
+    -- used <leader>gm to see the related git commit msg
     use 'rhysd/git-messenger.vim'
-    use {'junegunn/fzf', run = './install --all' }     -- Fuzzy Searcher
-    use {'junegunn/fzf.vim'}
+    -- if executable('sk')
+    --     Plug 'lotabout/skim', { 'dir': '~/.skim', 'do': './install' }
+    --     Plug 'lotabout/skim.vim'
+    --     command! -bang -nargs=* Rg call fzf#vim#rg_interactive(<q-args>, fzf#vim#with_preview('right:50%:hidden', 'alt-h'))
+    -- else
+        use {'junegunn/fzf', run = './install --all' }     -- Fuzzy Searcher
+        use {'junegunn/fzf.vim'}
+    -- endif
+    -- use  'scrooloose/nerdtree', { 'on': 'NERDTreeToggle' }
+    -- use 'jistr/vim-nerdtree-tabs', { 'on': 'NERDTreeToggle' }
+    -- use 'xuyuanp/nerdtree-git-plugin', { 'on': 'NERDTreeToggle' }
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
+    use 'tpope/vim-rhubarb' -- required by fugitive to :Gbrowse
+    use 'Raimondi/delimitMate'
+    use 'Yggdroot/indentLine'
+    -- Plug 'Shougo/vimproc.vim', {'do': g:make}
+    -- Vim-Session
+    -- Plug 'xolox/vim-misc'
+    -- Plug 'xolox/vim-session'
+    -- c/c++
+    -- Plug 'vim-scripts/c.vim', {'for': ['c', 'cpp']}
+    -- Plug 'ludwig/split-manpage.vim'
+    -- html
+    -- Plug 'tpope/vim-haml'
+    -- " Plug 'mattn/emmet-vim'
+    -- rust
+    -- Plug 'racer-rust/vim-racer', { 'for': 'rust' }
+    -- Plug 'rust-lang/rust.vim', { 'for': 'rust' }
+    -- Plug 'timonv/vim-cargo', { 'for': 'rust' }
+    -- " TODO: get debugger working <06-12-20, Heiko Riemer> "
+    -- Plug 'vim-scripts/Conque-GDB', { 'for': ['c', 'cpp', 'rust'] }
+    -- see colorschemes http://bytefluent.com/vivify/
+    use 'flazz/vim-colorschemes'
+    use 'plasticboy/vim-markdown'
+    use 'mzlogin/vim-markdown-toc'
+    use 'ntpeters/vim-better-whitespace'
+    -- Plug 'davidhalter/jedi-vim', { 'for': 'python' }
+    -- Plug 'ekalinin/Dockerfile.vim', { 'for': 'dockerfile' }
+    -- todo:slow Plug 'chrisbra/csv.vim'
+    -- Plug 'jmcantrell/vim-virtualenv', { 'for': 'python' }
+    -- Plug 'joonty/vdebug', { 'for': 'python' }
+    -- " todo:slow Plug 'ap/vim-css-color'
+    -- " syntax for plantuml and make command
+    use 'aklt/plantuml-syntax'
+    -- " Plug 'sjurgemeyer/vim-plantuml', { 'for': 'plantuml' }
+    use 'nathanalderson/yang.vim'
+    -- " todo:slow Plug 'bling/vim-bufferline'
+    -- " interesting but not yet configured
+    -- Plug 'sjl/gundo.vim', { 'on': [] }
+    -- " Git commit browser (:GV)
+    -- Plug 'junegunn/gv.vim'
+    -- " works with tabular to format markdown tables when pressing "|"
+    -- " todo: fix Plug 'quentindecock/vim-cucumber-align-pipes'
+    -- " immediate preview
+    -- " Vimwiki - http://thedarnedestthing.com/vimwiki%20cheatsheet
+    -- " Plug 'vimwiki/vimwiki', { 'for': 'markdown' }
+    use 'vimwiki/vimwiki'
+    -- " Latex
+    -- " Plug 'vim-latex/vim-latex', { 'for': 'tex' }
+    -- " Plug 'lervag/vimtex'
+    use 'donRaphaco/neotex'
+    use 'benmills/vimux'
+    use 'tpope/vim-repeat'
+    use 'tpope/vim-unimpaired'
+    use 'tpope/vim-speeddating'
+    use 'easymotion/vim-easymotion'
+    use 'vim-scripts/matchit.zip'
+    -- " Plug 'rstacruz/sparkup'
+    -- " Plug 'mjbrownie/hackertyper.vim'
+    -- " Plug 'sirtaj/vim-openscad'
+    -- Plug '907th/vim-auto-save'
+    use 'will133/vim-dirdiff'
+    use 'christoomey/vim-conflicted'
+    use 'christoomey/vim-sort-motion'
+    use 'christoomey/vim-tmux-navigator'
+    use 'brooth/far.vim'
+    -- " allows opening files at specific location - e.g. /tmp/bal:10:2
+    -- Plug 'wsdjeg/vim-fetch'
+    -- " Plug 'henrik/vim-open-url'
+    use 'romainl/vim-cool'
+    -- " TODO: buggy
+    -- " Plug 'stsewd/gx-extended.vim'
+    -- " better encryption plugin - requires: https://github.com/jedisct1/encpipe
+    use 'hauleth/vim-encpipe'
+    -- " floating windows
+    use 'machakann/vim-highlightedyank'
+    use 'voldikss/vim-floaterm'
+    use 'stsewd/fzf-checkout.vim'
+    -- " edit JIRA issues in vim
+    -- use 'dddelispt42/vira', { 'do': './install.sh' }
+    -- " automatically set the root directory
+    use 'airblade/vim-rooter'
+
+    -- " TODO: test this alternative to Ale/Ycm  <02-05-20, Heiko Riemer> "
+    -- " Plug 'mattn/vim-lsp-settings'
+    -- " Plug 'prabirshrestha/async.vim'
+    -- " Plug 'prabirshrestha/asyncomplete-lsp.vim'
+    -- " Plug 'prabirshrestha/asyncomplete.vim'
+    -- " Plug 'prabirshrestha/vim-lsp'
+    -- " Plug 'thomasfaingnaert/vim-lsp-ultisnips'
+
+    --" Plug 'CoatiSoftware/vim-sourcetrail'
+
   end,
   config = {
     _display = {
