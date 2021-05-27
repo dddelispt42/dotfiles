539e9c11b85f76d7815381f020abfda54fe78b6b
Riemer, Heiko <heiko@eheiko.net>
2021-05-27 12:43:41 +0100

    fix dotfile zsh bindkey to ALT-x to make it work

--- shell/.config/shell/keybinding.sh
+++ shell/.config/shell/keybinding.sh
@@ -6,4 +6,4 @@ bindkey '^[[A' fzf-history-widget # Ctrl-R to reverse search history w/ fzf
 # bindkey '^E' fzf-history-widget # Ctrl-A jump to end of line
 bindkey '^[[H' beginning-of-line
 bindkey '^[[F' end-of-line
-bindkey -s '^[d' 'fconf\n'
+bindkey -s '^[x' 'fconf\n' # Alt-X to edit a config file
