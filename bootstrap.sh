cb249f280e2dd2a61a6cc02e81dba3df1475906a
Heiko Riemer <heiko@eheiko.net>
2021-10-04 17:11:14 +0100

    add a bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 doas pacman -S stow zsh git openssh starship trash-cli zoxide fzf neovim
 [[ -n ${LANG+z} ]] || (doas nvim /etc/locale.gen; doas locale-gen)
 mkdir -p "$HOME/dev/heiko"
@@ -8,3 +9,13 @@ test -d dotfiles || git clone ssh://git@git/home/git/dotfiles.git
 test -d bootstrap || git clone ssh://git@git/home/git/bootstrap.git
 cd dotfiles || exit
 ./install.sh
+=======
+doas pacman -Syu
+doas pacman -S stow zsh git openssh
+mkdir -p "$HOME/.ssh"
+chmod 600
+test -s "$HOME/.ssh/id_rsa_low" || ssh-keygen -C "$(whoami)@$(hostname)-$(date -I)-low" -f "$_"
+test -s "$HOME/.ssh/id_rsa_high" || ssh-keygen -C "$(whoami)@$(hostname)-$(date -I)-high" -f "$_"
+echo "Add public key to authorized_keys for git server:"
+cat "$HOME/.ssh/id_rsa_low.pub"
+>>>>>>> 0221e97 (add a bootstrap.sh script)
