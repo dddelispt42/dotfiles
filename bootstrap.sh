05a95cd34880a4aa971ea1a096559c4993bbb352
Heiko Riemer <heiko@eheiko.net>
2021-10-13 11:15:09 +0000

    include default ssh key generation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-doas pacman -Syu
-doas pacman -S stow zsh git openssh
-mkdir -p "$HOME/.ssh"
-chmod 600
-test -s "$HOME/.ssh/id_rsa_low" || ssh-keygen -C "$(whoami)@$(hostname)-$(date -I)-low" -f "$_"
-test -s "$HOME/.ssh/id_rsa_high" || ssh-keygen -C "$(whoami)@$(hostname)-$(date -I)-high" -f "$_"
-echo "Add public key to authorized_keys for git server:"
-cat "$HOME/.ssh/id_rsa_low.pub"
+doas pacman -S stow zsh git openssh starship trash-cli zoxide fzf neovim
+[[ ! -z ${LANG+z} ]] || (doas nvim /etc/locale.gen; doas locale-gen)
+cd "$HOME/dev/heiko"
+git clone ssh://git@git/home/git/dotfiles.git
+cd dotfiles
+./install.sh
