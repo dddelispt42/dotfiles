d190acd7a902405e76adf634ca2e8f195bc0ed43
Riemer, Heiko <heiko@eheiko.net>
2021-12-16 22:28:11 +0000

    improve polybar colors

--- polybar/.config/polybar/weather.sh
+++ polybar/.config/polybar/weather.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CACHEFILE="$HOME/.cache/wttr.$USER"
 # curl 'wttr.in/Lisbon?format=%l+%c+%t+%h+%p+%o+%w+%m'
-curl -s 'wttr.in/Lisbon?format=%c+%t+%h+%p+%o+%w' | tee "$CACHEFILE" || cat "$CACHEFILE" || echo "???"
+curl -s 'wttr.in/Lisbon?format=%c+%t+%h+%p+%o+%w' | sed 's/  / /g' | tee "$CACHEFILE" || cat "$CACHEFILE" || echo "???"
