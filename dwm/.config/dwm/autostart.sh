c8ddf2f26a5d2d25ab7d131fe89c244ed2f42025
Riemer, Heiko <heiko@eheiko.net>
2022-07-28 20:35:00 +0100

    update autostart

--- dwm/.config/dwm/autostart.sh
+++ dwm/.config/dwm/autostart.sh
@@ -3,6 +3,9 @@
 HOST=$(hostname)
 TERMI="${TERMINAL:-alacritty}"
 
+xrdb -merge "${XDG_CONFIG_HOME:-$HOME/.config}/X11/.Xresources"
+xrdb "${XDG_CONFIG_HOME:-$HOME/.config}/X11/.Xresources"
+
 if [ "$HOST" == "work" ]; then
 	# MONITOR=$(xrandr --listmonitors | grep -v "Monitors:" | sed 's/.*\s//;1q')
 	# xrandr --output $MONITOR --mode 1920x1080 --rate 60
@@ -12,10 +15,17 @@ if [ "$HOST" == "work" ]; then
 	VBoxClient --seamless
 	# VBoxClient --display
 	VBoxClient --checkhostversion
+	nohup "$TERMI" --class "$TERMI - heiko@localhost" --title "$TERMI - heiko@localhost" &
+	nohup "$TERMI" --class "$TERMI - heiko@ed" --title "$TERMI - heiko@ed" &
+	nohup "$TERMI" --class "$TERMI - heiko@ed2" --title "$TERMI - heiko@ed2" &
+	nohup "$TERMI" --class "$TERMI - heiko@backup" --title "$TERMI - heiko@backup" &
+else
+	nohup "$TERMI" --class "$TERMI - heiko@localhost" --title "$TERMI - heiko@localhost" &
+	nohup "$TERMI" --class "$TERMI - heiko@ed" --title "$TERMI - heiko@ed" &
+	nohup "$TERMI" --class "$TERMI - heiko@ed2" --title "$TERMI - heiko@ed2" &
+	nohup "$TERMI" --class "$TERMI - heiko@backup" --title "$TERMI - heiko@backup" &
 fi
 
-xrdb -merge "${XDG_CONFIG_HOME:-$HOME/.config}/X11/.Xresources"
-xrdb "${XDG_CONFIG_HOME:-$HOME/.config}/X11/.Xresources"
 # sxhkd -c ${XDG_CONFIG_HOME:-$HOME/.config}/sxhkd/sxhkdrc &
 "${XDG_CONFIG_HOME:-$HOME/.config}/polybar/launch.sh" "$HOST"
 "${XDG_CONFIG_HOME:-$HOME/.config}/dunst/launch.sh" &
@@ -24,14 +34,27 @@ udiskie -A -t &
 nohup xfce4-power-manager &
 # nohup nextcloud &
 # trayer --edge top --align center --expand false --width 5 --distance 20 &
-nohup clipmenud &
-nohup brave &
-nohup qutebrowser &
-nohup "$TERMI" --class "$TERMI - heiko@localhost" --title "$TERMI - heiko@localhost" &
-nohup "$TERMI" --class "$TERMI - heiko@ed" --title "$TERMI - heiko@ed" &
-nohup "$TERMI" --class "$TERMI - heiko@ed2" --title "$TERMI - heiko@ed2" &
-nohup "$TERMI" --class "$TERMI - heiko@backup" --title "$TERMI - heiko@backup" &
-nohup anki &
-nohup tbb &
-nohup signal-desktop &
-nohup onboard &
+if command -v clipmenud >/dev/null; then
+	nohup clipmenud &
+fi
+if command -v brave >/dev/null; then
+	nohup brave &
+fi
+if command -v qutebrowser >/dev/null; then
+	nohup qutebrowser &
+fi
+if command -v anki >/dev/null; then
+	nohup anki &
+fi
+if command -v tbb >/dev/null; then
+	nohup tbb &
+fi
+if command -v signal-desktop >/dev/null; then
+	nohup signal-desktop &
+fi
+if command -v onboard >/dev/null; then
+	nohup onboard &
+fi
+if command -v xsuspender >/dev/null; then
+	nohup xsuspender &
+fi
