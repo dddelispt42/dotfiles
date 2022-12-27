8754efbb6b875d7943635d951a42c9401ac73a49
Riemer, Heiko <heiko@eheiko.net>
2022-12-27 11:32:38 +0000

    switch from lspinstaller to mason/null-ls

---
+++ nvim/.config/nvim/lua/cursorline_config.lua
@@ -0,0 +1,18 @@
+-- luacheck: globals vim
+local cursorline_ok, cursorline = pcall(require, "nvim-cursorline")
+if not cursorline_ok then
+    return
+end
+
+cursorline.setup {
+  cursorline = {
+    enable = true,
+    timeout = 1000,
+    number = false,
+  },
+  cursorword = {
+    enable = true,
+    min_length = 3,
+    hl = { underline = true },
+  }
+}
