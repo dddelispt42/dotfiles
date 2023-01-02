022ea9300d3a8d812b462ebc63b798e87e3e743f
Riemer, Heiko <heiko@eheiko.net>
2023-01-02 08:21:00 +0000

    finalize orgmode tuning

--- nvim/.config/nvim/lua/orgmode_config.lua
+++ nvim/.config/nvim/lua/orgmode_config.lua
@@ -1,56 +1,86 @@
 -- luacheck: globals vim
-local orgmode_ok, orgmode = pcall(require, "neorg")
-if not orgmode_ok then
-    return
+local neorg_ok, neorg = pcall(require, 'neorg')
+if not neorg_ok then
+  return
 end
 
+-- local orgbullets_ok, orgbullets = pcall(require, 'org-bullets')
+-- if not orgbullets_ok then
+--   return
+-- end
+
+local orgmode_ok, orgmode = pcall(require, 'orgmode')
+if not orgmode_ok then
+  return
+end
+
+orgmode.setup_ts_grammar()
+
+-- orgbullets.setup()
 
 orgmode.setup {
-    load = {
-        ["core.defaults"] = {},
-        ["core.highlights"] = {},
-        ["core.norg.dirman"] = {
-            config = {
-                workspaces = {
-                    work = "~/notes/work",
-                    home = "~/notes/home",
-                }
-            }
-        },
-        ["core.norg.journal"] = {},
-        ["core.norg.concealer"] = {},
-        ["core.norg.qol.toc"] = {},
-        ["core.norg.qol.todo_items"] = {
-            config = { task_cycle = '<S-Space>' }
-        },
-        ["core.syntax"] = {},
-        ["core.tangle"] = {},
-        ['core.norg.completion'] = {
-            config = { engine = 'nvim-cmp' }
-        },
-        ['core.export'] = {},
-        ['core.export.markdown'] = {},
-        ['core.presenter'] = {
-            config = { zen_mode = 'zen-mode' }
-        },
-        ['core.integrations.treesitter'] = {},
-        ['core.norg.esupports.metagen'] = {},
-    }
+  org_todo_keywords = { 'TODO(t)', 'WAITING(w)', 'DELEGATED(s)', '|', 'DONE(d)', 'REJECTED(r)' },
+  -- org_agenda_files = { '~/Sync/work/notes/*', '~/Sync/work/notes/**/*.org' },
+  org_agenda_files = { '~/Sync/work/notes/**/*.org' },
+  org_default_notes_file = '~/notes/capture.org',
+  org_capture_templates = {
+    t = {
+      description = 'Todo',
+      template = '* TODO %? :%n:\n %U',
+    },
+    d = {
+      description = 'Delegated task',
+      template = '* DELEGATED %? :who:\n %U',
+    },
+    m = {
+      description = 'Meeting',
+      template = '* MEETING %?\n %U',
+      target = '~/notes/work/journal.org',
+    },
+    j = {
+      description = 'Journal',
+      template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
+      target = '~/notes/work/journal.org',
+    },
+  },
+  mappings = {
+    global = {
+      org_agenda = { 'gA', '<Leader>oa' },
+      org_capture = { 'gC', '<Leader>oc' },
+    },
+  },
 }
 
-vim.api.nvim_set_keymap("n", "<leader>oc", "<cmd>Neorg gtd capture<CR>", {
-  noremap = true,
-  silent = true,
-})
-vim.api.nvim_set_keymap("n", "<leader>oi", "<cmd>Neorg workspace home<CR>", {
-  noremap = true,
-  silent = true,
-})
-vim.api.nvim_set_keymap("n", "<leader>oe", "<cmd>Neorg gtd edit<CR>", {
-  noremap = true,
-  silent = true,
-})
-vim.api.nvim_set_keymap("n", "<leader>ov", "<cmd>Neorg gtd views<CR>", {
-  noremap = true,
-  silent = true,
-})
+-- TODO: remove this if switched to orgmode completely
+neorg.setup {
+  load = {
+    ['core.defaults'] = {},
+    ['core.highlights'] = {},
+    ['core.norg.dirman'] = {
+      config = {
+        workspaces = {
+          work = '~/notes/work',
+          home = '~/notes/home',
+        },
+      },
+    },
+    ['core.norg.journal'] = {},
+    ['core.norg.concealer'] = {},
+    ['core.norg.qol.toc'] = {},
+    ['core.norg.qol.todo_items'] = {
+      config = { task_cycle = '<S-Space>' },
+    },
+    ['core.syntax'] = {},
+    ['core.tangle'] = {},
+    ['core.norg.completion'] = {
+      config = { engine = 'nvim-cmp' },
+    },
+    ['core.export'] = {},
+    ['core.export.markdown'] = {},
+    ['core.presenter'] = {
+      config = { zen_mode = 'zen-mode' },
+    },
+    ['core.integrations.treesitter'] = {},
+    ['core.norg.esupports.metagen'] = {},
+  },
+}
--- nvim/.config/nvim/lua/treesitter_config.lua
+++ nvim/.config/nvim/lua/treesitter_config.lua
@@ -1,53 +1,53 @@
 -- luacheck: globals vim
-local ts_ok, ts = pcall(require, "nvim-treesitter.configs")
+local ts_ok, ts = pcall(require, 'nvim-treesitter.configs')
 if not ts_ok then
-    return
+  return
 end
 
-vim.api.nvim_exec(
-    [[
-    set foldmethod=expr
-    set foldexpr=nvim_treesitter#foldexpr()
-    ]],
-    false
-)
+-- vim.api.nvim_exec(
+--     [[
+--     set foldmethod=expr
+--     set foldexpr=nvim_treesitter#foldexpr()
+--     ]],
+--     false
+-- )
 
-ts.setup({
-    -- A list of parser names, or "all"
-    -- ensure_installed = {"c", "lua", "rust", "norg"},
-    ensure_installed = "all",
+ts.setup {
+  -- A list of parser names, or "all"
+  -- ensure_installed = {"c", "lua", "rust", "norg"},
+  ensure_installed = 'all',
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
 
-    -- List of parsers to ignore installing (for "all")
-    -- ignore_install = { "javascript" },
+  -- List of parsers to ignore installing (for "all")
+  -- ignore_install = { "javascript" },
 
-    highlight = {
-        enable = true,
+  highlight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'gnn',
+      scope_incremental = 'gnc',
+      node_decremental = 'gnd',
     },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "gnn",
-            scope_incremental = "gnc",
-            node_decremental = "gnd",
-        },
-    },
-    indent = {
-        enable = true,
-    },
-    rainbow = {
+  },
+  -- indent = {
+  --   enable = true,
+  -- },
+  rainbow = {
     enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
-  }
-})
+  },
+}
