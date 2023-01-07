f321e482f3533f6b19b2ecf60a7f85499dbc9538
Heiko Riemer <heiko@eheiko.net>
2023-01-07 03:06:39 +0000

    remove no longer maintained rainbow plugin

--- nvim/.config/nvim/lua/treesitter_config.lua
+++ nvim/.config/nvim/lua/treesitter_config.lua
@@ -1,94 +1,86 @@
 -- luacheck: globals vim
-local ts_ok, ts = pcall(require, 'nvim-treesitter.configs')
+local ts_ok, ts = pcall(require, "nvim-treesitter.configs")
 if not ts_ok then
-  return
+	return
 end
 
 vim.api.nvim_exec(
-  [[    set foldmethod=expr
+	[[    set foldmethod=expr
     set foldexpr=nvim_treesitter#foldexpr()
     ]],
-  false
+	false
 )
 
-ts.setup {
-  -- A list of parser names, or "all"
-  -- ensure_installed = {"c", "lua", "rust", "norg"},
-  ensure_installed = 'all',
+ts.setup({
+	-- A list of parser names, or "all"
+	-- ensure_installed = {"c", "lua", "rust", "norg"},
+	ensure_installed = "all",
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-  -- ignore_install = { "javascript" },
+	-- List of parsers to ignore installing (for "all")
+	-- ignore_install = { "javascript" },
 
-  highlight = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'gnn',
-      scope_incremental = 'gnc',
-      node_decremental = 'gnd',
-    },
-  },
-  indent = {
-    enable = true,
-  },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>sa'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>sA'] = '@parameter.inner',
-      },
-    },
-  },
-}
+	highlight = { enable = true },
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "gnn",
+			scope_incremental = "gnc",
+			node_decremental = "gnd",
+		},
+	},
+	indent = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["aa"] = "@parameter.outer",
+				["ia"] = "@parameter.inner",
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>sa"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>sA"] = "@parameter.inner",
+			},
+		},
+	},
+})
