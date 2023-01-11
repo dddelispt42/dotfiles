9384968d3ff9e8aae0484461a54f78243cbf7d3b
Heiko Riemer <heiko@eheiko.net>
2023-01-11 17:26:14 +0000

    add java lsp

--- nvim/.config/nvim/lua/null-ls_config.lua
+++ nvim/.config/nvim/lua/null-ls_config.lua
@@ -25,7 +25,7 @@ mason_null_ls.setup({
 		"protolint",
 		"refactoring",
 		"shellcheck",
-		-- 'ruff',
+		"ruff",
 		"clang_format",
 		"shfmt",
 		"sqlformat",
@@ -57,7 +57,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettier.with({
 			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
 		}),
-		-- null_ls.builtins.formatting.ruff,
+		null_ls.builtins.formatting.ruff,
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.formatting.shfmt,
