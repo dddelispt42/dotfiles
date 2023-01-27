031130a51bd52eecb4e84dd3da5531d51cd678a3
Heiko Riemer <heiko@eheiko.net>
2023-01-27 00:05:43 +0000

    switch from packer to lazy (neovim)

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -1,39 +1,43 @@
 -- luacheck: globals vim
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    is_bootstrap = true
-    vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-    vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    }
 end
+vim.opt.rtp:prepend(lazypath)
+--
+-- Define leader key
+vim.g.mapleader = ',' -- 'vim.g' sets global variables
+vim.g.maplocalleader = ','
 
-require('packer').startup(function(use)
-    -- the packet manager for nvim
-    use 'wbthomason/packer.nvim'
+require('lazy').setup {
     -- Fuzzy Finder (files, lsp, etc)
-    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-    -- use "mhinz/vim-startify"
-    -- use "dstein64/vim-startuptime"
-    use 'norcalli/nvim-colorizer.lua'
-    use 'folke/zen-mode.nvim'
-    use {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
+    'norcalli/nvim-colorizer.lua',
+    'folke/zen-mode.nvim',
+    {
         'glacambre/firenvim',
-        run = function()
+        build = function()
             vim.fn['firenvim#install'](0)
         end,
-    }
-    use {
+    },
+    {
         'kyazdani42/nvim-tree.lua',
         config = function()
             require('nvim-tree').setup {}
         end,
-    }
-    use { -- LSP Configuration & Plugins
+    },
+    { -- LSP Configuration & Plugins
         'neovim/nvim-lspconfig',
-        requires = {
+        dependencies = {
             -- Automatically install LSPs to stdpath for neovim
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
@@ -42,25 +46,25 @@ require('packer').startup(function(use)
             -- Additional lua configuration, makes nvim stuff amazing
             'folke/neodev.nvim',
         },
-    }
-    use 'jose-elias-alvarez/null-ls.nvim'
-    use 'jayp0521/mason-null-ls.nvim'
-    use { 'LostNeophyte/null-ls-embedded' }
-    use 'jayp0521/mason-nvim-dap.nvim'
-    -- use 'glepnir/lspsaga.nvim'
-    -- use 'wbthomason/lsp-status.nvim'
-    -- use 'nvim-lua/lsp_extensions.nvim'
-    use 'simrat39/rust-tools.nvim'
-    use {
+    },
+    'jose-elias-alvarez/null-ls.nvim',
+    'jayp0521/mason-null-ls.nvim',
+    'LostNeophyte/null-ls-embedded',
+    'jayp0521/mason-nvim-dap.nvim',
+    -- 'glepnir/lspsaga.nvim',
+    -- 'wbthomason/lsp-status.nvim',
+    -- 'nvim-lua/lsp_extensions.nvim',
+    'simrat39/rust-tools.nvim',
+    {
         'saecki/crates.nvim',
-        requires = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             require('crates').setup()
         end,
-    }
-    use {
+    },
+    {
         'folke/trouble.nvim',
-        requires = 'kyazdani42/nvim-web-devicons',
+        dependencies = 'kyazdani42/nvim-web-devicons',
         config = function()
             require('trouble').setup {
                 auto_open = true, -- automatically open the list when you have diagnostics
@@ -71,10 +75,11 @@ require('packer').startup(function(use)
                 mode = 'document_diagnostics',
             }
         end,
-    }
-    use { -- Autocompletion
+    },
+    { -- Autocompletion
         'hrsh7th/nvim-cmp',
-        requires = {
+        event = 'InsertEnter',
+        dependencies = {
             'L3MON4D3/LuaSnip',
             'f3fora/cmp-spell',
             'hrsh7th/cmp-buffer',
@@ -88,47 +93,46 @@ require('packer').startup(function(use)
             'saadparwaiz1/cmp_luasnip',
             -- "pontusk/cmp-vimwiki-tags"
         },
-    }
-    use 'rafamadriz/friendly-snippets'
-    use { -- Highlight, edit, and navigate code
+    },
+    'rafamadriz/friendly-snippets',
+    { -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
-        run = function()
+        build = function()
             pcall(require('nvim-treesitter.install').update { with_sync = true })
         end,
-    }
-    use { -- Additional text objects via treesitter
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        after = 'nvim-treesitter',
-    }
-    use 'nvim-treesitter/playground'
-    use {
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+    },
+    'nvim-treesitter/playground',
+    {
         'nvim-treesitter/completion-treesitter',
-        run = function()
+        build = function()
             vim.cmd [[TSUpdate]]
         end,
-    }
-    use 'aklt/plantuml-syntax'
-    -- use("Sol-Ponz/plantuml-previewer.nvim")
-    use 'sidebar-nvim/sidebar.nvim'
-    use 'sidebar-nvim/sections-dap'
-    use 'mfussenegger/nvim-dap'
-    use { 'rcarriga/nvim-dap-ui', requires = { 'mfussenegger/nvim-dap' } }
-    use 'mfussenegger/nvim-dap-python'
-    use 'nvim-telescope/telescope-dap.nvim'
-    use {
+    },
+    'aklt/plantuml-syntax',
+    -- 'Sol-Ponz/plantuml-previewer.nvim',
+    'sidebar-nvim/sidebar.nvim',
+    'sidebar-nvim/sections-dap',
+    'mfussenegger/nvim-dap',
+    { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
+    'mfussenegger/nvim-dap-python',
+    'nvim-telescope/telescope-dap.nvim',
+    {
         'theHamsta/nvim-dap-virtual-text',
-        run = function()
+        build = function()
             vim.g.dap_virtual_text = true
         end,
-    }
-    use {
+    },
+    {
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end,
-    }
-    use 'tpope/vim-sleuth'
-    use {
+    },
+    'tpope/vim-sleuth',
+    {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
             require('indent_blankline').setup {
@@ -139,55 +143,52 @@ require('packer').startup(function(use)
                 show_trailing_blankline_indent = true,
             }
         end,
-    }
-    use 'tpope/vim-surround' -- Surround text objects easily
-    use 'TimUntersberger/neogit'
-    use 'lewis6991/gitsigns.nvim'
-    use 'akinsho/git-conflict.nvim'
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-rhubarb' -- required by fugitive to :Gbrowse
-    use 'ThePrimeagen/refactoring.nvim'
-    use 'sindrets/diffview.nvim'
-    use {
+    },
+    'tpope/vim-surround', -- Surround text objects easily
+    'TimUntersberger/neogit',
+    'lewis6991/gitsigns.nvim',
+    'akinsho/git-conflict.nvim',
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb', -- required by fugitive to :Gbrowse
+    'ThePrimeagen/refactoring.nvim',
+    'sindrets/diffview.nvim',
+    {
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    }
-    use 'yamatsum/nvim-cursorline'
-    use 'ellisonleao/gruvbox.nvim'
-    -- use({
+        dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
+    },
+    'yamatsum/nvim-cursorline',
+    {
+        'ellisonleao/gruvbox.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd [[colorscheme gruvbox]]
+        end,
+    },
+    -- {
     -- 	"akinsho/bufferline.nvim",
-    -- 	requires = "kyazdani42/nvim-web-devicons",
+    -- 	dependencies = "kyazdani42/nvim-web-devicons",
     -- 	config = function()
     -- 		require("bufferline").setup({})
     -- 	end,
-    -- })
-    use 'romgrk/barbar.nvim'
-    -- " interesting but not yet configured
-    -- " Git commit browser (:GV)
-    -- Plug 'junegunn/gv.vim'
-    -- " works with tabular to format markdown tables when pressing "|"
-    -- " todo: fix Plug 'quentindecock/vim-cucumber-align-pipes'
-    -- " immediate preview
-    -- " Vimwiki - http://thedarnedestthing.com/vimwiki%20cheatsheet
-    -- " Plug 'vimwiki/vimwiki', { 'for': 'markdown' }
-    use 'vimwiki/vimwiki'
-    -- " Latex
-    -- " Plug 'vim-latex/vim-latex', { 'for': 'tex' }
-    -- " Plug 'lervag/vimtex'
-    -- use 'donRaphaco/neotex'
-    -- use 'benmills/vimux'
-    use 'tpope/vim-repeat'
-    use 'tpope/vim-unimpaired'
-    use 'tpope/vim-speeddating'
-    use {
+    -- },
+    'romgrk/barbar.nvim',
+    {
+        'vimwiki/vimwiki',
+        ft = 'vimwiki',
+    },
+    'tpope/vim-repeat',
+    'tpope/vim-unimpaired',
+    'tpope/vim-speeddating',
+    {
         'phaazon/hop.nvim',
         branch = 'v1', -- optional but strongly recommended
         config = function()
             -- you can configure Hop the way you like here; see :h hop-config
             require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
         end,
-    }
-    use {
+    },
+    {
         'folke/which-key.nvim',
         config = function()
             require('which-key').setup {
@@ -196,47 +197,49 @@ require('packer').startup(function(use)
                 -- refer to the configuration section below
             }
         end,
-    }
+    },
     -- " Plug 'mjbrownie/hackertyper.vim'
-    use 'will133/vim-dirdiff'
-    use 'christoomey/vim-conflicted'
-    use 'christoomey/vim-sort-motion'
-    use {
+    'will133/vim-dirdiff',
+    'christoomey/vim-conflicted',
+    'christoomey/vim-sort-motion',
+    {
         'aserowy/tmux.nvim',
         config = function()
             require('tmux').setup()
         end,
-    }
-    use 'brooth/far.vim'
+    },
+    'brooth/far.vim',
     -- " allows opening files at specific location - e.g. /tmp/bal:10:2
-    use 'wsdjeg/vim-fetch'
+    'wsdjeg/vim-fetch',
     -- " Plug 'henrik/vim-open-url'
-    -- use 'romainl/vim-cool'
-    -- " better encryption plugin - requires: https://github.com/jedisct1/encpipe
-    use 'hauleth/vim-encpipe'
+    -- 'romainl/vim-cool',
+    -- " better encryption plugin - dependencies: https://github.com/jedisct1/encpipe
+    'hauleth/vim-encpipe',
     -- " floating windows
-    -- use 'machakann/vim-highlightedyank'
-    use 'voldikss/vim-floaterm'
+    -- 'machakann/vim-highlightedyank',
+    'voldikss/vim-floaterm',
     -- edit JIRA issues in vim
-    -- use "n0v1c3/vira"
-    -- use 'dddelispt42/vira', { 'do': './install.sh' }
-    use 'n0v1c3/vira'
+    -- 'n0v1c3/vira0,
+    -- 'dddelispt42/vira', { 'do': './install.sh', }
+    -- 'n0v1c3/vira',
     -- " automatically set the root directory
-    use 'airblade/vim-rooter'
-    -- use "github/copilot.vim"
-    use {
+    'airblade/vim-rooter',
+    -- 'github/copilot.vim',
+    {
         'nvim-neorg/neorg',
-        run = ':Neorg sync-parsers', -- This is the important bit!
-        -- after = "nvim-treesitter", -- You may want to specify Telescope here as well
-        requires = 'nvim-lua/plenary.nvim',
-        -- ft = 'norg',
-    }
-    use 'nvim-orgmode/orgmode'
-    use { 'michaelb/sniprun', run = 'bash ./install.sh' }
-    use 'dhruvasagar/vim-table-mode'
-    use 'rest-nvim/rest.nvim'
+        ft = 'norg',
+        build = ':Neorg sync-parsers', -- This is the important bit!
+        dependencies = 'nvim-lua/plenary.nvim',
+    },
+    {
+        'nvim-orgmode/orgmode',
+        ft = 'org',
+    },
+    { 'michaelb/sniprun', build = 'bash ./install.sh' },
+    'dhruvasagar/vim-table-mode',
+    'rest-nvim/rest.nvim',
     -- Remove the `use` here if you're using folke/lazy.nvim.
-    use {
+    {
         'Exafunction/codeium.vim',
         config = function()
             -- Change '<C-g>' here to any keycode you like.
@@ -244,37 +247,5 @@ require('packer').startup(function(use)
                 return vim.fn['codeium#Accept']()
             end, { expr = true })
         end,
-    }
-
-    -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-    local has_plugins, plugins = pcall(require, 'custom.plugins')
-    if has_plugins then
-        plugins(use)
-    end
-
-    if is_bootstrap then
-        require('packer').sync()
-    end
-end)
-
--- When we are bootstrapping a configuration, it doesn't
--- make sense to execute the rest of the init.lua.
---
--- You'll need to restart nvim, and then it will work.
-if is_bootstrap then
-    print '=================================='
-    print '    Plugins are being installed'
-    print '    Wait until Packer completes,'
-    print '       then restart nvim'
-    print '=================================='
-    return
-end
-
--- Automatically source and re-compile packer whenever you save this file
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-    command = 'source <afile> | PackerCompile',
-    group = packer_group,
-    -- TODO: do for plugin.lua
-    pattern = vim.fn.expand '$MYVIMRC',
-})
+    },
+}
