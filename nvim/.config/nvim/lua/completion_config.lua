97f38c28b5e95c7bf56a8e1da734e9ce04330686
Heiko Riemer <heiko@eheiko.net>
2023-06-02 14:25:46 +0100

    add lsp signature helper

--- nvim/.config/nvim/lua/completion_config.lua
+++ nvim/.config/nvim/lua/completion_config.lua
@@ -45,6 +45,7 @@ cmp.setup {
         end, { 'i', 's' }),
     },
     sources = {
+        { name = 'nvim_lsp_signature_help' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'treesitter' },
