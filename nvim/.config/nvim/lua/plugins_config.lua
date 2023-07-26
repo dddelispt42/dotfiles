afedb89def189ba413dd0c01d50f2987be7e05b7
Heiko Riemer <heiko@eheiko.net>
2023-07-26 16:53:22 +0100

    switch from vimscript surround to lua version

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -166,7 +166,16 @@ require('lazy').setup({
             }
         end,
     },
-    'tpope/vim-surround', -- Surround text objects easily
+    {
+        'kylechui/nvim-surround',
+        version = '*', -- Use for stability; omit to use `main` branch for the latest features
+        event = 'VeryLazy',
+        config = function()
+            require('nvim-surround').setup {
+                -- Configuration here, or leave empty to use defaults
+            }
+        end,
+    },
     'NeogitOrg/neogit',
     'lewis6991/gitsigns.nvim',
     'akinsho/git-conflict.nvim',
