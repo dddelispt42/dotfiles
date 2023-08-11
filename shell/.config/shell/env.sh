bf5069d5ea1b4b61ab2d40122c081ec544a57fd5
Heiko Riemer <heiko@eheiko.net>
2023-08-11 21:47:04 +0100

    switch from st/alacritty to foot (at least on wayland)

--- shell/.config/shell/env.sh
+++ shell/.config/shell/env.sh
@@ -102,8 +102,9 @@ export CLICOLORS=1
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 # default terminal emulator
-export TERMINAL=st
-command -v alacritty >/dev/null && export TERMINAL="alacritty"
+export TERMINAL=xterm
+command -v st >/dev/null && export TERMINAL="st"
+command -v foot >/dev/null && test -n "$WAYLAND_DISPLAY" && export TERMINAL="foot"
 
 # my BIB file
 export BIB=$HOME/Documents/uni.bib
