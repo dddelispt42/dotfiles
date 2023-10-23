49704bf7367d64a9d168c08d5364dbcbc45cf2be
Heiko Riemer <heiko@eheiko.net>
2023-10-23 12:42:26 +0100

    add link to file open (defined in lf)

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -253,7 +253,7 @@ require('lazy').setup({
             { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
             { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
             { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "TS Search" },
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
     },
