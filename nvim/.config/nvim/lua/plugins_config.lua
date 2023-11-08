25efba5026d1bf05488b7b203785022f6a7c7d44
Heiko Riemer <hriemer@infinera.com>
2023-11-08 18:37:46 +0000

    bufferline diagnostics not working

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -230,17 +230,7 @@ require('lazy').setup({
         'akinsho/bufferline.nvim',
         dependencies = 'kyazdani42/nvim-web-devicons',
         config = function()
-            require('bufferline').setup {
-                --- count is an integer representing total count of errors
-                --- level is a string "error" | "warning"
-                --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
-                --- this should return a string
-                --- Don't get too fancy as this function will be executed a lot
-                diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                    local icon = level:match 'error' and ' ' or ' '
-                    return ' ' .. icon .. count
-                end,
-            }
+            require('bufferline').setup {}
         end,
     },
     -- 'romgrk/barbar.nvim',
