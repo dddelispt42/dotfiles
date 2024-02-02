1407a594aff8fd6aa9e930537bd3895bb926d3c6
Heiko Riemer <heiko@eheiko.net>
2024-02-02 18:48:11 +0000

    update firenvim setup

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -47,9 +47,12 @@ require('lazy').setup({
     -- },
     {
         'glacambre/firenvim',
+        -- Lazy load firenvim
+        -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+        lazy = not vim.g.started_by_firenvim,
         build = function()
-            vim.fn['firenvim#install'](0)
-        end,
+            vim.fn["firenvim#install"](0)
+        end
     },
     {
         'kyazdani42/nvim-tree.lua',
