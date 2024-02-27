b9fc8ee20383be3c360a975273a962c95c4fddca
Heiko Riemer <mail@eheiko.net>
2024-02-27 22:34:07 +0000

    add telescope undo

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -610,7 +610,7 @@ require('lazy').setup({
     reset_packpath = true, -- reset the package path to improve startup time
     performance = {
         rtp = {
-            reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+            reset = false, -- reset the runtime path to $VIMRUNTIME and your config directory
             ---@type string[]
             paths = {}, -- add any custom paths here that you want to includes in the rtp
             ---@type string[] list any plugins you want to disable here
