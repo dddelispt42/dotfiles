419fc1ad5553713c8070e6f4d22b87c1d78439d2
Heiko Riemer <heiko@eheiko.net>
2024-03-03 11:28:28 +0000

    switch wayland lockscreen from swaylock to waylock (issues with pw input)

--- hyprland/.config/hypr/idle.sh
+++ hyprland/.config/hypr/idle.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# swayidle -w \
+# 	timeout 600 'swaylock -f -c 000000' \
+# 	timeout 3600 'loginctl suspend' \
+# 	before-sleep 'swaylock -f -c 000000'
 swayidle -w \
-	timeout 600 'swaylock -f -c 000000' \
+	timeout 900 'waylock' \
 	timeout 3600 'loginctl suspend' \
-	before-sleep 'swaylock -f -c 000000'
+	before-sleep 'waylock'
