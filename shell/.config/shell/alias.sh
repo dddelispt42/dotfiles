61f3ac785cea578c2a23a0ee9e25cd6c2b575802
Heiko Riemer <mail@eheiko.net>
2024-04-08 16:58:42 +0100

    add alias for X and Wayland launch

--- shell/.config/shell/alias.sh
+++ shell/.config/shell/alias.sh
@@ -162,3 +162,5 @@ alias thokr='thokr -w 200 -l english10k'
 alias xbindkeys=xbindkeys -f "$XDG_CONFIG_HOME"/xbindkeys/config
 alias svn=svn --config-dir "$XDG_CONFIG_HOME/subversion"
 alias jui='docker run --rm -it -v $XDG_CONFIG_HOME/jira.d/:/config/ pmjohann/go-jira-ui'
+alias X="startx"
+alias W="dbus-run-session Hyprland"
