017fa081fbac867091a6863372e004241c57620e
Riemer, Heiko <heiko@eheiko.net>
2024-05-27 10:12:38 +0100

    C-up/down is for nvim not for tmux swap

--- nvim/.config/nvim/lua/keymaps_config.lua
+++ nvim/.config/nvim/lua/keymaps_config.lua
@@ -59,10 +59,10 @@ map('t', '<C-j>', '<C-\\><C-N><C-w>j', { silent = true })
 map('t', '<C-k>', '<C-\\><C-N><C-w>k', { silent = true })
 map('t', '<C-l>', '<C-\\><C-N><C-w>l', { silent = true })
 -- Resize with arrows
-map('n', '<C-Up>', ':resize +2<CR>', { noremap = true, silent = true })
-map('n', '<C-Down>', ':resize -2<CR>', { noremap = true, silent = true })
-map('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
-map('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+map('n', '<C-Up>', ':resize +3<CR>', { noremap = true, silent = true })
+map('n', '<C-Down>', ':resize -3<CR>', { noremap = true, silent = true })
+map('n', '<C-Left>', ':vertical resize -3<CR>', { noremap = true, silent = true })
+map('n', '<C-Right>', ':vertical resize +3<CR>', { noremap = true, silent = true })
 map('n', '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>', { desc = 'Redraw / clear hlsearch / diff update' })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
