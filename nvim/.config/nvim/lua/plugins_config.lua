5d3bb12b11eba65afc50b52991bc2fa5788cde7e
Heiko Riemer <heiko@eheiko.net>
2024-06-13 18:27:45 +0100

    add fugit nvim

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -404,6 +404,26 @@ require('lazy').setup({
         event = 'LspAttach',
     },
     {
+        'SuperBo/fugit2.nvim',
+        opts = {
+            width = 70,
+            external_diffview = true, -- tell fugit2 to use diffview.nvim instead of builtin implementation.
+        },
+        dependencies = {
+            'MunifTanjim/nui.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'nvim-lua/plenary.nvim',
+            {
+                'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
+                dependencies = { 'stevearc/dressing.nvim' },
+            },
+        },
+        cmd = { 'Fugit2', 'Fugit2Diff', 'Fugit2Graph' },
+        keys = {
+            { '<leader>F', mode = 'n', '<cmd>Fugit2<cr>' },
+        },
+    },
+    {
         'sindrets/diffview.nvim',
         cmd = {
             'DiffviewOpen',
