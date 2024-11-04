fd41823d9a66eced3eff26131eaae3d0cdf5fcee
Heiko Riemer <heiko@eheiko.net>
2024-11-04 10:29:11 +0000

    update install instructions for firenvim

--- nvim/.config/nvim/lua/plugins_config.lua
+++ nvim/.config/nvim/lua/plugins_config.lua
@@ -45,15 +45,7 @@ require('lazy').setup({
     --     },
     --     cmd = { 'ZenMode', 'Twilight' },
     -- },
-    {
-        'glacambre/firenvim',
-        -- Lazy load firenvim
-        -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-        lazy = not vim.g.started_by_firenvim,
-        build = function()
-            vim.fn['firenvim#install'](0)
-        end,
-    },
+    { 'glacambre/firenvim', build = ":call firenvim#install(0)" },
     {
         'kyazdani42/nvim-tree.lua',
         cmd = 'NvimTreeToggle',
