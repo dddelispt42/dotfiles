d6aeece308789b69d53b3a9fd5ef49df6a9bfad1
Heiko Riemer <heiko@eheiko.net>
2024-11-21 15:11:47 +0000

    add nix config

--- install.sh
+++ install.sh
@@ -131,23 +131,22 @@ migrate_to_clean "$HOME"/.myclirc "${XDG_CONFIG_HOME}"/myclirc
 stow -vS -t "$HOME"/ X11
 stow -vS -t "$HOME"/ alacritty
 stow -vS -t "$HOME"/ atuin
-stow -vS -t "$HOME"/ bat
-bat cache --build 1>/dev/null
-stow -vS -t "$HOME"/ imv
+stow -vS -t "$HOME"/ bat && bat cache --build 1>/dev/null
 stow -vS -t "$HOME"/ bottom
-stow -vS -t "$HOME"/ gitui
 stow -vS -t "$HOME"/ broot
 stow -vS -t "$HOME"/ direnv
 stow -vS -t "$HOME"/ dunst
 stow -vS -t "$HOME"/ dwm
 stow -vS -t "$HOME"/ foot
 stow -vS -t "$HOME"/ git
-stow -vS -t "$HOME"/ lf
+stow -vS -t "$HOME"/ gitui
 stow -vS -t "$HOME"/ hyprland
+stow -vS -t "$HOME"/ imv
+stow -vS -t "$HOME"/ lf
 stow -vS -t "$HOME"/ mpv
 stow -vS -t "$HOME"/ neofetch
+stow -vS -t "$HOME"/ nix
 stow -vS -t "$HOME"/ nvim
-stow -vS -t "$HOME"/ zellij
 stow -vS -t "$HOME"/ paru
 stow -vS -t "$HOME"/ polybar
 stow -vS -t "$HOME"/ qutebrowser
@@ -162,14 +161,15 @@ stow -vS -t "$HOME"/ swappy
 stow -vS -t "$HOME"/ tmux
 stow -vS -t "$HOME"/ topgrade
 stow -vS -t "$HOME"/ vifm
-stow -vS -t "$HOME"/ ytfzf
-stow -vS -t "$HOME"/ xplr
-stow -vS -t "$HOME"/ xsuspender
-stow -vS -t "$HOME"/ zathura
-stow -vS -t "$HOME"/ zsh
 stow -vS -t "$HOME"/ waybar
 stow -vS -t "$HOME"/ wezterm
+stow -vS -t "$HOME"/ xplr
+stow -vS -t "$HOME"/ xsuspender
 stow -vS -t "$HOME"/ yazi
+stow -vS -t "$HOME"/ ytfzf
+stow -vS -t "$HOME"/ zathura
+stow -vS -t "$HOME"/ zellij
+stow -vS -t "$HOME"/ zsh
 
 # add links
 test -d "${HOME}/opt" || mkdir -p "$_"
