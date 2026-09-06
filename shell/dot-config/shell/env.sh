62a48b915f4f541155cfffae192dad865de198f5
Heiko Riemer <heiko@eheiko.net>
2026-09-06 22:03:29 +0000

    some security improvements

--- shell/dot-config/shell/env.sh
+++ shell/dot-config/shell/env.sh
@@ -25,8 +25,7 @@ export XDG_TEMPLATES_DIR="$HOME/media/temp"
 export XDG_VIDEOS_DIR="$HOME/media/videos"
 
 # don't put duplicate lines or lines starting with space in the history.
-# See bash(1) for more options
-# HISTCONTROL=ignoreboth
+HISTCONTROL=ignoreboth
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=5000
